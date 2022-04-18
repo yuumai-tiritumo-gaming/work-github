05f3d83bc239b1944e4a1f3c6b00dd6923767ed4
TAKERU-KURIYAMA <takerukuriyama@gmail.com>
2022-04-18 10:06:35 +0000

    [機能だけ]public_sign_in

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,6 +18,9 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def after_sign_in_path_for(resource)
+    public_root_path
+  end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
