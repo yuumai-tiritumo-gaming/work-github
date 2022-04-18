c9964030d0765930a4223d13223f5ed0ac449ba4
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-18 04:25:50 +0000

    [add]config/applicationrb

--- config/application.rb
+++ config/application.rb
@@ -20,3 +20,6 @@ module NaganoCake
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
+  config.i18n.default_locale = :ja
+  config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
