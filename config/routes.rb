743d66c4ef54e52df5a170e360273be9ef8d8f34
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-20 06:06:51 +0000

    [Fix]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,18 +11,18 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
 namespace :public, path: '/' do
     root to: "items#top"
-    resources "items",                       only: [:show,:index]
-    get       "/about"                       => "items#about"
-    get       "/customers/confirmation"      => "customers#confirmation"
-    get       "/mypage"                      => "customers#show"
-    patch     "/mypage"                      => "customers#unsubscribe"
-    resource  "customers",                   only: [:edit,:update]
-    resources "addresses",                   only: [:index,:edit,:create,:destroy,:update]
-    resources "orders",                      only: [:index,:show,:new,:create]
-    get       "/orders/check"                => "orders#check"
-    get       "/orders/conform"              => "orders#conform"
-    resources "cart_items",                  only: [:index,:create,:destroy,:update]
-    delete    "/cart_items/clear"            => "cart_items#clear"
+    resources "items",                        only: [:show,:index]
+    get       "/about"                        => "items#about"
+    get       "/customer/confirmation"        => "customers#confirmation"
+    get       "/mypage"                       => "customers#show"
+    patch     "/mypage"                       => "customers#unsubscribe"
+    resource  "customers", path: '/customer', only: [:edit,:update]
+    resources "addresses",                    only: [:index,:edit,:create,:destroy,:update]
+    resources "orders",                       only: [:index,:show,:new,:create]
+    get       "/orders/check"                 => "orders#check"
+    get       "/orders/conform"               => "orders#conform"
+    resources "cart_items",                   only: [:index,:create,:destroy,:update]
+    delete    "/cart_items/clear"             => "cart_items#clear"
   end
 
 
