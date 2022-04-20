58cd4ec21026476e242516e78b1a441ec23a47d4
TAKERU-KURIYAMA <takerukuriyama@gmail.com>
2022-04-20 02:19:59 +0000

    [all_fn_admin_items]admin_items_new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,8 +11,13 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_items_path
+    @genre = Genre.all
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render :new
+    end
+
   end
 
   def show
@@ -26,8 +31,12 @@ class Admin::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to admin_item_path(@item)
+    @genre = Genre.all
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,14 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
 
   has_one_attached :item_image
-  
+
+  validates :name, presence: true
+  validates :body, presence: true
+  validates :item_image, presence: true
+  validates :genre_id, presence: true
+  validates :non_tax_price, presence: true
+  validates :is_selling, presence: true
+
   def add_tax_price
     (self.non_tax_price * 1.08).round
   end
