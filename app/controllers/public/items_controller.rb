d284dabc90fdf67e17782195383be579c5b8daf2
TAKERU-KURIYAMA <takerukuriyama@gmail.com>
2022-04-20 15:07:31 +0000

    [index]public_items

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,13 +1,15 @@
 class Public::ItemsController < ApplicationController
   def top
     @genres = Genre.all
-    @items = Item.order(created_at: :desc).limit(4)
+    @items = Item.order(created_at: :desc).limit(4) 
   end
 
   def about
   end
 
   def index
+    @genres = Genre.all
+    @items = Item.page(params[:page]).per(8)
   end
 
   def show
