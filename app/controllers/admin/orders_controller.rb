ccc31a840cce6f9e708082263a458cca1cb1160c
TAKERU-KURIYAMA <takerukuriyama@gmail.com>
2022-04-21 09:00:59 +0000

    testまえ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,6 +3,9 @@ class Admin::OrdersController < ApplicationController
   end
 
   def index
+    @orders = Order.order("created_at DESC").page(params[:page])
+    @customer = Customer.find(order.customer_id)
+    @order_items = @customer.order_items
   end
 
   def update
