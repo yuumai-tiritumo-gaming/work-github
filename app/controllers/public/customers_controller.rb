3a576e036d49e5ee98f721ec62d496dd7fc08668
iro-999 <999.iroiroiro@gmail.com>
2022-04-21 04:57:04 +0000

    [Finish]public-customer-confirmation

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,11 +20,11 @@ class Public::CustomersController < ApplicationController
   end
 
   def unsubscribe
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
     @customer.update(is_active: false)
     reset_session
     flash[:notice] = "退会処理を実行いたしました"
-    redirect_to root_path
+    redirect_to public_root_path
   end
 
   private
