e0373327324070fc5f6ad887610c9af03b4f2913
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-21 07:58:20 +0000

    [Add]Seed.rb

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,5 +3,5 @@ class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
-  validates :quantity, comparison: { greater_than: 0 }
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 end
