6ed6ace7b724c5871faa6cf4a16e9be459e4d7b0
iro-999 <999.iroiroiro@gmail.com>
2022-04-22 03:40:33 +0000

    [Add]public-address-edit

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -14,9 +14,16 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to public_mypage_path(@customer), notice: "会員情報を更新しました"
+    else
+      render "edit"
+    end
   end
 
   private
