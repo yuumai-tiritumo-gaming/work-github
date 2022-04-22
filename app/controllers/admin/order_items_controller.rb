5a35e6fc8feb5b389954bc5ecf04230107d1e2a7
TAKERU-KURIYAMA <99077553+TAKERU-KURIYAMA@users.noreply.github.com>
2022-04-22 15:24:32 +0900

    Update order_items_controller.rb

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,7 +2,6 @@ class Admin::OrderItemsController < ApplicationController
   def update
     @order_item = OrderItem.find(params[:id])
     @order_item.update(making_status: params[:making_status].to_i)
-    @order = Order.find(params[:id])
-    redirect_to admin_order_path(@order)
+    redirect_to request.referer
   end
 end
