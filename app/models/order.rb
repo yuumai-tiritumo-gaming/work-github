8875f9f843e2d8a2d3131a4533ef866b8fac5e86
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-22 06:38:42 +0000

    show/index

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
 
   belongs_to :customer
-  has_many :order_items, dependent: :destroy
+  has_many   :order_items, dependent: :destroy
 
   validates :postal_code, length: { is: 7 }
 end
