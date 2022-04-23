cfb56f1623afddef63b96a4f717ea976a0c3eceb
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-23 11:22:12 +0000

    [fix].to_s(:delimited)

--- app/helpers/public/cart_items_helper.rb
+++ app/helpers/public/cart_items_helper.rb
@@ -22,4 +22,15 @@ module Public::CartItemsHelper
     @price.to_s(:delimited)
   end
 
+   def total_price_undelimited(cart_item_all)
+    @price = 0
+    cart_item_all.each do |cart_items|
+      items  = Item.find(cart_items.item_id)
+      tax_in = ((items.non_tax_price)*1.1).floor
+      num    = (tax_in*(cart_items.quantity)).floor
+      @price += num
+    end
+    @price
+   end
+
 end
