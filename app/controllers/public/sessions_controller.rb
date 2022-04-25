f5b7efa0c7b5e2fa52bb8e36ebd9cda51b9cca7e
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-25 08:48:26 +0000

    [fix]admin-session-layout

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,25 +24,25 @@ class Public::SessionsController < Devise::SessionsController
     public_root_path
   end
 
-  
+
   def after_sign_out_path_for(resource)
     public_root_path
   end
 
-  
+
   def reject_customer
     @customer = Customer.find_by(email: params[:customer][:email].downcase)
     if @customer
-      
+
       if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
         flash[:error] = "退会済みです。"
         redirect_to new_customer_session_path
       end
-      
+
     else
       flash[:error] = "メールアドレスを入力してください。"
     end
-    
+
   end
 
   # If you have extra params to permit, append them to the sanitizer.
