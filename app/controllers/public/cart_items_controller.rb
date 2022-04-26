759bacb44cb8b5bb42b0b0a68194bb181f30f383
TAKERU-KURIYAMA <takerukuriyama@gmail.com>
2022-04-26 05:03:56 +0000

    cart_item_before_action_fix

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,20 @@
 class Public::CartItemsController < ApplicationController
-  before_action :customer_signed_in?, only: [:create]
+  before_action :valid_customer?, only: [:create]
 
   def create
     @cart_item = CartItem.new(cart_item_params)
     if @cart_item.save
       redirect_to action: "index"
     else
+      binding.pry
       @genres = Genre.all
       @item = Item.find(@cart_item.item_id)
-      render "public/items/show"
+      if customer_signed_in?
+        flash[:alert] = "カート一覧から修正してください"
+      else
+        flash[:alert] = "商品購入には会員登録またはログインが必要です"
+      end
+      redirect_to request.referer
     end
   end
 
@@ -52,4 +58,9 @@ class Public::CartItemsController < ApplicationController
     params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
   end
 
+  def valid_customer?
+    unless customer_signed_in?
+      redirect_to new_customer_session_path
+    end
+  end
 end
