6cddf42ac3d0db2108d2521ca11b43661f44fa0b
YubeChang <yubel.kabaddi.tiramis@gmail.com>
2022-04-27 12:38:29 +0000

    [finish]fix

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,8 +1,15 @@
 class Admin::OrderItemsController < ApplicationController
   before_action :valid_admin?
+
   def update
     @order_item = OrderItem.find(params[:id])
     @order_item.update(making_status: params[:making_status].to_i)
+    if params[:making_status] == "2"
+      @order_item.order.update(trade_status: 2 )
+    elsif params[:making_status] == "3" && @order_item.order.order_items.all? {|order_item| order_item.making_status == OrderItem.making_statuses.key(3)}
+      @order_item.order.update(trade_status: 3 )
+    end
     redirect_to request.referer
   end
+
 end
